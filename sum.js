6ed5d2a5d4bab996b24571f6df16c2affc1b015a
waqarshabir <waqarshabir13543@gmail.com>
2026-04-09 13:33:33 +0500

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,98 @@
+//let number=5
+//if(number%2 === 0){
+  //  console.log(true)
+//}
+//else{
+  //  false
+//console.log(false)
+//}
+
+
+//last element of array
+
+
+//let arr = [10, 20, 30, 40];
+//let last = arr[arr.length - 1];
+//console.log(last);
+
+
+//reversed of array
+
+
+//let arr = [10, 20, 30, 40];
+//let reversed = arr.reverse();
+//console.log(reversed)
+
+
+//find length
+
+//let arr = [5, 10, 15];
+//console.log(arr.length);
+
+
+
+let arr = [1,2,3,4,5,6,7,8]
+for(let i=0;i<=arr.length;i++){  
+  if(arr[i] % 2 === 0){         
+    console.log(arr[i])
+  }
+}
+
+
+
+// let arr = [0,1,2,3,4,50,68,79,54]
+// for(let a=0; a<=arr.length;a++){ 
+//   if(arr[a]%2 !==0){        
+//     console.log(arr[a])
+//   }
+// }
+
+
+
+
+// let arr = [40,50,30,34,87,56,20,98,65,34,56,0,12,23]
+// let sum = 0;   
+// for(let b=0; b <=arr.length; b++){ 
+//   sum= sum + arr[b];     
+// }
+// console.log(sum);
+
+
+// let arr = [5,3,2,4,1]
+// arr.sort((a , b) => a - b
+// );
+// console.log(arr);
+ 
+let arrr = [1,2,3,4,5,6,7,8,9]
+arrr.sort((a,b)=>b-a);
+console.log(arrr);
+
+
+
+
+
+
+// let array = [1,2,3,4,5,6,7,8]
+// for(let i=0; i<=array.length;i++){
+//   if(array[i]%2===0){
+//     console.log(array[i]);
+    
+//   }
+// }
+
+
+
+// let arr1 = [20,30,40,50,60]
+// let sum=0
+// for(let a=0;a<=arr1.length;a++){
+//   sum = sum + arr1[a]
+// }
+// console.log(sum);
+
+
+// let arr2=[1,2,8,4,5,6]
+// arr2.sort((a,b) => a-b);
+//   console.log(arr2)
+  
+// let
+
